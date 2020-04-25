7d39d030393068b3affbf2275fd704f4d0c699ab
Anant Tandon <tandonanant8@gmail.com>
2020-04-25 13:54:57 +0000

    ENV file added to hide password of database

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect('mongodb+srv://tescoDb:Pass1234!@mytescodb-67a8e.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.MongoDB_Connection, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
